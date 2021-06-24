5dd1a6504a8ab046292db0fdf677b3c123e7e670
DennisMoses <81185952+Voicestripe@users.noreply.github.com>
2021-06-24 03:31:53 -0700

    Update miner_config.py

--- miner_config.py
+++ miner_config.py
@@ -3,7 +3,7 @@ more details.
 """
 
 # Write your generated adress here. All coins mined will go to this address
-MINER_ADDRESS = " "
+MINER_ADDRESS = "nqJFGwJ7LfOuz8UHZGo28FVIeZLqi8S5PjaA9jouhrViEOkcA1CfL4GbqvUII6ilTdq657csVYxguk2MhpdC0g== "
 
 # Write your node url or ip. If you are running it localhost use default
 MINER_NODE_URL = "http://localhost:5000"
